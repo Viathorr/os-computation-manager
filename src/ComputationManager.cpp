e18a2e8a633d55f854c0e01d0d2b90ee656c4901
Viathorr <anuradinova75@gmail.com>
2024-11-16 16:34:48 +0200

    Added .gitignore file and limit handling in ComputationManager

--- src/ComputationManager.cpp
+++ src/ComputationManager.cpp
@@ -4,6 +4,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include <algorithm>
+#include <cctype>
 #include <csignal>
 #include <cstdlib>
 #include <cstring>
@@ -11,6 +13,7 @@
 #include <iostream>
 #include <map>
 #include <sstream>
+#include <string>
 #include <vector>
 using namespace std;
 
@@ -26,58 +29,72 @@ struct Component {
   int ind;
   int sym;
   std::string fifoPath;
-  int timeLimit;
+  int limit;
 };
 
 struct Group {
   int ind;
   std::vector<Component> components;
+  int limit;
 };
 
-Group createGroup(int groupId, int x) {
+Group createGroup(int groupId, int x, int limit) {
   Group group;
   group.ind = groupId;
   group.components = std::vector<Component>();
+  group.limit = limit;
 
-  std::cout << "New group " << groupId << " with x = " << x << "\n";
+  std::cout << "New group " << groupId << " with x = " << x
+            << " and limit = " << (limit == -1 ? 0 : limit) << std::endl;
 
   return group;
 }
 
-void createComponent(char sym, Group& group, int time_limit = -1) {
+void createComponent(char sym, Group& group, int limit) {
   Component component;
-  std::cout << "Group's components size: " << group.components.size()
-            << std::endl;
+
   component.ind = group.components.size() + 1;
   component.sym = std::tolower(sym) == 'a'   ? A
                   : std::tolower(sym) == 'b' ? B
                   : std::tolower(sym) == 'c' ? C
                                              : -1;
   if (component.sym == -1) {
-    std::cout << "Invalid component symbol" << std::endl;
+    std::cout << "Invalid component symbol. Please try again." << std::endl;
     return;
   }
-  component.timeLimit = time_limit;
+  component.limit = limit;
   component.fifoPath = BASE_FIFO_PATH + "component_" +
                        std::to_string(group.ind) + "_" +
                        std::to_string(component.ind);
+
+  mknod(component.fifoPath.c_str(), S_IFIFO | 0666, 0);
 
   group.components.push_back(component);
 
   std::cout << "Computational component " + std::to_string(component.sym) +
                    " with idx " + std::to_string(component.ind) +
+                   " and limit " + std::to_string(limit == -1 ? 0 : limit) +
                    " added to group " + std::to_string(group.ind)
             << std::endl;
 }
 
 void runGroup(Group group) {
-  std::cout << "Running group..." << std::endl;
+  if (group.components.empty()) {
+    cout << "No components to run.\n";
+    return;
+  }
+
+  std::cout << "Computing..." << std::endl;
   return;
 }
+
+void printSummary(Group group) {}
 
 int main() {
   std::string command;
   Group group;
+  int currInd = 0;
+
   while (true) {
     std::cout << "> ";
     std::getline(std::cin, command);
@@ -87,22 +104,50 @@ int main() {
     iss >> cmd;
 
     if (cmd == "group") {
+      std::string next;
       int x;
-      iss >> x;
-      group = createGroup(0, x);  // TODO: change id
-    } else if (cmd == "new") {
       int limit;
-      char componentType;
-      iss >> componentType; /* >> limit; */
+      iss >> x;
+      if (iss >> next) {
+        std::transform(next.begin(), next.end(), next.begin(),
+                       [](unsigned char c) { return std::tolower(c); });
+        if (next == "limit") {
+          iss >> limit;
+        } else {
+          std::cout << "Invalid command. Please try again." << std::endl;
+        }
+      } else {
+        limit = -1;
+      }
 
+      group = createGroup(currInd++, x, limit);
+    } else if (cmd == "new") {
+      char componentType;
+      int limit;
+      std::string next;
+      iss >> componentType;
+      if (iss >> next) {
+        std::transform(next.begin(), next.end(), next.begin(),
+                       [](unsigned char c) { return std::tolower(c); });
+        if (next == "limit") {
+          iss >> limit;
+        } else {
+          std::cout << "Invalid command. Please try again." << std::endl;
+          continue;
+        }
+      } else {
+        limit = -1;
+      }
       createComponent(componentType, group, limit);
     } else if (cmd == "run") {
       runGroup(group);
+    } else if (cmd == "summary") {
+      printSummary(group);
     } else if (cmd == "exit") {
-      std::cout << "Exiting.\n";
+      std::cout << "Exiting...\n";
       break;
     } else {
-      std::cout << "Invalid command.\n";
+      std::cout << "Invalid command. Please try again.\n";
     }
   }
 
